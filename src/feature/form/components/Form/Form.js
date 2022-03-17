23da84d32a19a795ec75098bc672c6dd9562041b
ohthatbaguk <natashabaguk@gmail.com>
2022-03-17 16:37:48 +0400

    Add initial state

--- src/feature/form/components/Form/Form.js
+++ src/feature/form/components/Form/Form.js
@@ -1,8 +1,14 @@
 import { Provider, useForm } from "src/feature/form";
 
-export default function Form({ children, onSubmit, validators, className }) {
+export default function Form({
+  initialData = {},
+  children,
+  onSubmit,
+  validators,
+  className,
+}) {
   const { values, changeHandler, errors, isTouched, handleSubmit, isValid } =
-    useForm({}, validators, onSubmit);
+    useForm(initialData, validators, onSubmit);
   return (
     <form className={className} onSubmit={handleSubmit}>
       <Provider value={{ values, errors, isTouched, changeHandler, isValid }}>
